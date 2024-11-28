0d6a425dc2d5e8b5ecfa9e4590effefffb9c1889
mnesicles <benjamindelgado.contacto@gmail.com>
2024-11-28 07:55:37 -0300

    add FisiksBodyController function

---
+++ src/FisiksBodyController.ts
@@ -0,0 +1,38 @@
+import { Fisiks2DVector } from "./Fisiks2DVector";
+import { FisiksBody } from "./FisiksBody";
+
+const keys: Record<string, boolean> = {
+    a: false,
+    s: false,
+    d: false,
+    w: false,
+};
+
+document.addEventListener('keydown', (event) => {
+    if (event.key in keys) {
+        keys[event.key] = true;
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key in keys) {
+        keys[event.key] = false;
+    }
+});
+
+export function FisiksBodyController(body: FisiksBody, secondsPassed: number, speed: number = 400) {
+    let dx = 0;
+    let dy = 0;
+
+    if (keys.a) dx -= 1;
+    if (keys.d) dx += 1;
+    if (keys.w) dy -= 1;
+    if (keys.s) dy += 1;
+
+    if (dx !== 0 || dy !== 0) {
+        const direction = Fisiks2DVector.Normalize(new Fisiks2DVector(dx, dy));
+        const velocity = Fisiks2DVector.ScalarMultiplication(speed * secondsPassed, direction);
+
+        body.Move(velocity);
+    }
+}
